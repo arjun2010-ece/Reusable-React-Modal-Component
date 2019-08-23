2539e114bbef82c9233fb10022ecc2868f95bf49
arjun2010.ece <arjun2010.ece@gmail.com>
2019-08-23 02:03:38 +0100

    Reusable React modal dialog box component built

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component,Fragment } from 'react'
 import './App.css';
+import Backdrop from './components/Backdrop';
+import Modal from './components/Modal';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    creating: false
+  };
+ 
+  startCreateEventHandler = () => {
+   this.setState({ creating: true });
+  }
+ 
+  modalConfirmHandler = () => {
+   this.setState({ creating: false });
+  }
+ 
+  modalCancelHandler = () => {
+   this.setState({ creating: false });
+  }
+  render() {
+    return (
+      <Fragment>
+        {this.state.creating && <Backdrop onCancel={this.modalCancelHandler} />}
+        {this.state.creating && <Modal title="Add Event" canCancel canConfirm
+              onCancel={this.modalCancelHandler} onConfirm={this.modalConfirmHandler}>
+        <p>Modal content</p>
+        </Modal>}
+        <div className="events-control">
+            <p>Share your own Events</p>
+            <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
+        </div>
+      </Fragment>
+    )
+  }
 }
+
 
 export default App;
